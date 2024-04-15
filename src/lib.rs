830559355538bfa3b7fcbb6cf0127087d1649657
5-23 <yhanbyeol6@gmail.com>
2024-04-15 17:53:10 +0900

    feat: publish

--- src/lib.rs
+++ src/lib.rs
@@ -14,117 +14,103 @@ pub enum Cell {
     Dead = 0,
     Alive = 1,
 }
-#[wasm_bindgen]
-pub struct Universe {
-    width: u32,
-    height: u32,
-    cells: Vec<Cell>,
+impl Cell {
+    pub fn toggle(&mut self) {
+        *self = match *self {
+            Cell::Dead => Cell::Alive,
+            Cell::Alive => Cell::Dead,
+        }
+    }
+    pub fn parse_int(&self) -> u8 {
+        match self {
+            Cell::Dead => 0,
+            Cell::Alive => 1,
+        }
+    }
 }
 
 #[wasm_bindgen]
-impl Universe {
-    pub fn new() -> Self {
-        let width = 64;
-        let height = 64;
+pub struct LifeOfGame {
+    size: usize,
+    cells: Vec<Vec<Cell>>,
+}
 
-        let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
-
+#[wasm_bindgen]
+impl LifeOfGame {
+    pub fn new(size: usize) -> Self {
         Self {
-            width,
-            height,
-            cells,
+            size,
+            cells: vec![vec![Cell::Dead; size]; size],
         }
     }
 
-    pub fn render(&self) -> String {
-        self.to_string()
+    pub fn toggle(&mut self, i: usize, j: usize) {
+        self.cells[i][j].toggle();
     }
+    pub fn draw(&self) {
+        let doc = web_sys::window().unwrap().document().unwrap();
+        let game = doc.get_element_by_id("game").unwrap();
+        game.set_inner_html("");
+        for i in 0..self.size {
+            let mut html = format!("{}<div>", game.inner_html());
+            for j in 0..self.size {
+                html = format!(
+                    "{}<div type='button' id='{i}-{j}' class='tile' {}></div>",
+                    html,
+                    if self.cells[i][j] == Cell::Alive {
+                        "alive"
+                    } else {
+                        ""
+                    },
+                );
+            }
 
-    fn get_index(&self, row: u32, column: u32) -> usize {
-        (row * self.width + column) as usize
+            game.set_inner_html(&format!("{}</div>", html.clone()));
+        }
     }
-
-    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-        let mut count = 0;
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-                if delta_row == 0 && delta_col == 0 {
-                    continue;
+    pub fn step(&mut self) {
+        let mut changes = vec![];
+        for i in 0..self.size {
+            for j in 0..self.size {
+                let mut neighbor = 0;
+                for k in 0..=2 {
+                    for l in 0..=2 {
+                        neighbor += self
+                            .cells
+                            .get(i + k - 1)
+                            .unwrap_or(&Vec::new())
+                            .get(j + l - 1)
+                            .unwrap_or(&Cell::Dead)
+                            .parse_int();
+                    }
+                    changes.push((
+                        i,
+                        j,
+                        if neighbor == 3 {
+                            Cell::Alive
+                        } else {
+                            Cell::Dead
+                        },
+                    ))
                 }
-
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                let idx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[idx] as u8;
-            }
-        }
-        count
-    }
-    pub fn tick(&mut self) {
-        let mut next = self.cells.clone();
-
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-                let live_neighbors = self.live_neighbor_count(row, col);
-
-                let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
-
-                next[idx] = next_cell;
             }
         }
 
-        self.cells = next;
-    }
-    pub fn width(&self) -> u32 {
-        self.width
-    }
-
-    pub fn height(&self) -> u32 {
-        self.height
-    }
-
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
-    }
-}
-
-use std::fmt;
-
-impl fmt::Display for Universe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-                write!(f, "{}", symbol)?;
-            }
-            write!(f, "\n")?;
+        for data in changes {
+            self.cells[data.0][data.1] = data.2;
         }
-
-        Ok(())
+    }
+    pub fn render(&self) -> String {
+        let mut result = String::new();
+        for i in &self.cells {
+            for j in i {
+                match j {
+                    Cell::Dead => result.push_str("◻️"),
+                    Cell::Alive => result.push_str("◼️"),
+                }
+            }
+            result.push_str("\n");
+        }
+        result
     }
 }
